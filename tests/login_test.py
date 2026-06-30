f944b5aec269cddf3afc6074d139cbb6b3f95e26
Angie Faustino <angie01roberto@gmail.com>
2026-06-30 16:59:11 +0800

    Run Selenium in headless mode

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
 
+# Configure Chrome for GitHub Actions
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
+
 # Launch Chrome browser
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=options)
 
 # Open the login page
 driver.get("https://practicetestautomation.com/practice-test-login/")
